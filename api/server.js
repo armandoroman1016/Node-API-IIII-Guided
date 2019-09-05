c7da5770d6bee14ebb7384bd82a94a01e2c7196e
Armando Roman <armandoroman1016@gmail.com>
2019-09-05 09:44:31 -0700

    .env added

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
 
+require('dotenv').config()
+
+console.log('env', process.env.NODE_ENV)
+
 const apiRouter = require('./api-router.js');
 
 const server = express();
