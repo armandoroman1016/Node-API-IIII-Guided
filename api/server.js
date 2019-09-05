de4c35e2fc043c3df86bed143e26fcfae3ceff9f
Armando Roman <armandoroman1016@gmail.com>
2019-09-05 10:17:00 -0700

    push before delpoyment

--- api/server.js
+++ api/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
 
-require('dotenv').config()
-
 console.log('env', process.env.NODE_ENV)
 
 const apiRouter = require('./api-router.js');
